10088cc3c411f3ffb289d004d09c9de910cbcc97
mahmadlebowski <lejazzman1@gmail.com>
2021-01-20 18:33:08 +0000

    Updates

---
+++ precache-manifest.5dcaf97d9d0efcdca46c9b02a6c26197.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b9ad6390940ac4fbbe7abb4dd912e3",
+    "url": "/galileo-front/index.html"
+  },
+  {
+    "revision": "edd436d1fa36a74e4600",
+    "url": "/galileo-front/static/css/2.1e385327.chunk.css"
+  },
+  {
+    "revision": "1ed859cca12c6d9912f5",
+    "url": "/galileo-front/static/css/main.b310fb38.chunk.css"
+  },
+  {
+    "revision": "edd436d1fa36a74e4600",
+    "url": "/galileo-front/static/js/2.7e67585e.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/galileo-front/static/js/2.7e67585e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed859cca12c6d9912f5",
+    "url": "/galileo-front/static/js/main.20c40885.chunk.js"
+  },
+  {
+    "revision": "f8c9184794618147bf36",
+    "url": "/galileo-front/static/js/runtime-main.e708d8b1.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/galileo-front/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/galileo-front/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/galileo-front/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/galileo-front/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/galileo-front/static/media/primeicons.b0f5d02f.eot"
+  }
+]);
